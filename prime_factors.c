920b129425c6b2f7d8f9e845ad626f7acb05e9c6
Brahim <berraji@protonmail.com>
2018-11-26 22:46:21 +0100

    linked list method

--- prime_factors.c
+++ prime_factors.c
@@ -4,6 +4,7 @@
 typedef struct prime_factor
 {
 	unsigned int factor;
+	unsigned int multiplicity;
 	struct prime_factor *next;
 }	prime_factor;
 
@@ -14,12 +15,20 @@ prime_factor	*prime_factors(unsigned int n)
 	prime_factor *start = malloc(sizeof(prime_factor));;
 	int i = 2;
 	int j = 0;
+	int mul = 0;
 	int m = n;
 	while (m != 1)
 	{
 		if (m % i == 0)
 		{
+			mul = 0;
+			while (m % i == 0)
+			{
+				mul++;
+				m /= i;
+			}
 			current->factor = i;
+			current->multiplicity = mul;
 			if (j > 0)
 				prev->next = current;
 			if (j == 1)
@@ -32,7 +41,6 @@ prime_factor	*prime_factors(unsigned int n)
 			current->next = malloc(sizeof(prime_factor));
 			prev = current;
 			current = current->next;
-			m /= i;
 			i = 2;
 			j++;
 		}
@@ -44,11 +52,11 @@ prime_factor	*prime_factors(unsigned int n)
 }
 int main()
 {
-	prime_factor *a = prime_factors(500);
+	prime_factor *a = prime_factors(100000);
 
 	while(a != NULL)
 	{
-		printf("%u\n",a->factor);
+		printf("factor is %u ,its multiplicity is %u\n",a->factor,a->multiplicity);
 		a = a->next;
 	}
 	return 0;
